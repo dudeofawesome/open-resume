c210f47b69389892c5a26cf6ba024a40b1431883
dudeofawesome <louis@orleans.io>
2022-02-07 22:56:35 +0000

    🚀 release

--- _next/static/m8wLl1_yPQZFWFXsbCv0I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b2e984c5-ed1725fd3921ab82.js","static/css/db4549295d78c815.css","static/chunks/pages/index-eba5805083ad6a18.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
