1c0c0d20f727792ff1a969786df076e7a5195700
Louis Orleans <louis@orleans.io>
2023-03-16 03:39:07 -0700

    🔨 build global style

--- example/src/pages/_app.tsx
+++ example/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 
+import '@dudeofawesome/open-resume/build/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
