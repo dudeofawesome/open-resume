500bf50e6d8a36a00af5122bcd02f70c5fac1363
Louis Orleans <louis@orleans.io>
2023-03-15 22:50:00 -0700

    🐛 set color defaults

--- open-resume/src/hooks/css-vars.ts
+++ open-resume/src/hooks/css-vars.ts
@@ -6,27 +6,24 @@ export function useCSSVars(data: ResumeData) {
   useEffect(() => {
     const style = document.documentElement.style;
     const c = data.theme.colors;
-    if (c.primary != null) style.setProperty('--color-primary', c.primary);
-    if (c.primary_muted != null)
-      style.setProperty('--color-primary-muted', c.primary_muted);
-    if (c.text != null) style.setProperty('--color-text', c.text);
-    if (c.text_dim != null) style.setProperty('--color-text-dim', c.text_dim);
-    if (c.paper_background != null)
-      style.setProperty('--color-paper-background', c.paper_background);
-    if (c.background != null)
-      style.setProperty('--color-background', c.background);
 
-    if (c.dark_text != null)
-      style.setProperty('--color-dark-text', c.dark_text);
-    if (c.dark_text_dim != null)
-      style.setProperty('--color-dark-text-dim', c.dark_text_dim);
-    if (c.dark_paper_background != null)
-      style.setProperty(
-        '--color-dark-paper-background',
-        c.dark_paper_background,
-      );
-    if (c.dark_background != null)
-      style.setProperty('--color-dark-background', c.dark_background);
+    style.setProperty('--color-primary', c.primary ?? '#d20000');
+    style.setProperty('--color-primary-muted', c.primary_muted ?? '#920000');
+    style.setProperty('--color-text', c.text ?? '#000');
+    style.setProperty('--color-text-dim', c.text_dim ?? '#666');
+    style.setProperty('--color-paper-background', c.paper_background ?? '#fff');
+    style.setProperty('--color-background', c.background ?? '#181a1b');
+
+    style.setProperty('--color-dark-text', c.dark_text ?? '#fff');
+    style.setProperty('--color-dark-text-dim', c.dark_text_dim ?? '#d6d2cd');
+    style.setProperty(
+      '--color-dark-paper-background',
+      c.dark_paper_background ?? '#323232',
+    );
+    style.setProperty(
+      '--color-dark-background',
+      c.dark_background ?? '#181a1b',
+    );
 
     return () => {
       style.removeProperty('--color-primary');
