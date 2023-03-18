3280dacf4c802e4814c5618e0abbcc71a0c991f5
Louis Orleans <louis@orleans.io>
2023-03-17 19:59:55 -0700

    ✅ fix jest

--- open-resume/jest.config.ts
+++ open-resume/jest.config.ts
@@ -1,15 +1,8 @@
-import nextJest from 'next/jest';
+import type { JestConfigWithTsJest } from 'ts-jest/dist/types';
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
-
-// Add any custom config to be passed to Jest
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig({
+const config: JestConfigWithTsJest = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
-});
+  preset: 'ts-jest',
+  extensionsToTreatAsEsm: ['.ts'],
+};
+export default config;
