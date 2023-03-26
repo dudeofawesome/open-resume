f1fc6a516a88098a64e03bba525b92af631e48ee
Louis Orleans <louis@orleans.io>
2023-03-25 20:06:13 -0700

    ♻️ rearrange prepub

--- open-resume/scripts/prepub.sh
+++ open-resume/scripts/prepub.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
 
-rm -rf build/
+main() {
+  rm -rf build/
 
-set -e
+  set -e
 
-npm run build
+  npm run build
+  test_files
+  echo "Built!"
+}
 
 # ensure that we built at least some of the files we expected.
+test_files() {
+  if [ ! -f "build/package.json" ]; then
+    echo "package.json was not found."
+    exit 1;
+  fi
 
-if [ ! -f "build/package.json" ]; then
-  echo "package.json was not found."
-  exit 1;
-fi
+  main="$(jq --raw-output '.main' "build/package.json")"
 
-main="$(jq --raw-output '.main' "build/package.json")"
+  if [ ! -f "build/$main" ]; then
+    echo "$main was not found."
+    exit 1;
+  fi
 
-if [ ! -f "build/$main" ]; then
-  echo "$main was not found."
-  exit 1;
-fi
+  main_def="$(basename "$main" '.js').d.ts"
+  if [ ! -f "build/$main_def" ]; then
+    echo "$main_def was not found."
+    exit 1;
+  fi
 
-main_def="$(basename "$main" '.js').d.ts"
-if [ ! -f "build/$main_def" ]; then
-  echo "$main_def was not found."
-  exit 1;
-fi
+  if [ ! -f "build/globals.css" ]; then
+    echo "globals.css was not found."
+    exit 1;
+  fi
+}
 
-if [ ! -f "build/globals.css" ]; then
-  echo "globals.css was not found."
-  exit 1;
-fi
+main
