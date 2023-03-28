a932e084ce93c80c5db260c43f4de428ce2cdb27
Louis Orleans <louis@orleans.io>
2023-03-27 19:04:22 -0700

    🐛 use icon

--- open-resume/src/components/head-data.tsx
+++ open-resume/src/components/head-data.tsx
@@ -16,13 +16,13 @@ export const HeadData: FC<ResumeData> = (props) => {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       {/* <link rel="manifest" href="manifest.json" /> */}
 
-      <link rel="icon" href="/icon.svg" />
+      <link rel="icon" href={props.about.image_url} />
       <meta name="theme-color" content={props.theme.colors.primary_muted} />
 
       <meta property="og:site_name" content={title} />
       <meta property="og:title" content={title} />
       <meta property="og:type" content="profile" />
-      <meta property="og:image" content="/icon.svg" />
+      <meta property="og:image" content={props.about.image_url} />
       {props.about.label != null && (
         <meta property="og:description" content={props.about.label} />
       )}
